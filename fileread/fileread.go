8aa97c5a69eaa4c79d06602a40bce8ce404fbe2e
Willian Kazuo <willian.kazuo@wearelayer.com>
2023-09-11 09:30:18 -0300

    test env

--- fileread/fileread.go
+++ fileread/fileread.go
@@ -1,8 +1,13 @@
 package fileread
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func ReadContent() []byte {
+	a := os.Getenv("TESTE")
+	fmt.Println("env test ", a)
 	f, err := os.ReadFile("./testfile.txt")
 	if err != nil {
 		panic(err)
